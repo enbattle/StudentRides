9cff83956921b85c7a8f951541add1f54c9f9134
Nathan <natesyun@gmail.com>
2019-11-03 01:00:30 -0500

    StudentRosterTable

---
+++ StudentDriver/src/components/student-roster.js
@@ -0,0 +1,81 @@
+import React, { Component } from 'react';
+import SimpleMap from './map.js';
+
+class StudentRoster extends Component {
+    constructor() {
+        super()
+        this.state = {
+            usersList: [{
+                firstName: 'Kristofer',
+                lastName: 'Kwan',
+                email: 'kristofer.kwan@gmail.com',
+                phoneNumber: '1111111111'
+            },
+            {
+                firstName: 'Cristofer',
+                lastName: 'Cwan',
+                email: 'cristofer.cwan@gmail.com',
+                phoneNumber: '2222222222'
+            },
+            {
+                firstName: 'Xristofer',
+                lastName: 'Xwan',
+                email: 'xristofer.xwan@gmail.com',
+                phoneNumber: '3333333333'
+            },
+            {
+                firstName: 'Yristofer',
+                lastName: 'Ywan',
+                email: 'yristofer.ywan@gmail.com',
+                phoneNumber: '4444444444'
+            }]
+        }
+    }
+
+    renderTableData() {
+        var results = [];
+        var fN = [];
+        var lN = [];
+        var em = [];
+        var pn = [];
+        for (var i=0; i < this.state.usersList.length; i++){
+            results.push(
+            <tr>
+                <td>{this.state.usersList[i].firstName}</td>
+                <td>{this.state.usersList[i].lastName}</td>
+                <td>{this.state.usersList[i].email}</td>
+                <td>{this.state.usersList[i].phoneNumber}</td>
+            </tr>
+            )
+        }
+
+        /*
+        var fN = this.state.usersList[:].firstName;
+        var lN = this.state.usersList[:].lastName;
+        var em = this.state.usersList[:].email;
+        var pn = this.state.usersList[:].phoneNumber;
+        */
+        return results
+    }
+    render() {
+        return (
+           <div>
+              <h1 id='title'>Student Roster</h1>
+              <table class="table table-dark">    
+                <thead>
+                    <tr>
+                    <th scope="col">First Name</th>
+                    <th scope="col">Last Name</th>
+                    <th scope="col">Email</th>
+                    <th scope="col">Phone Number</th>
+                    </tr>
+                </thead>               
+                <tbody>
+                    {this.renderTableData()}
+                </tbody>
+              </table>
+           </div>
+        )
+     }
+}
+export default StudentRoster;
